218147d07ce9490b5263816766d9a75e64863603
michael.wan <michael.wan@ichub.com>
2023-02-06 17:37:03 +0800

    2023-02-06 17:36 if条件判断练习提交

---
+++ 20230206/if判断年龄问题.py
@@ -0,0 +1,23 @@
+"""
+给定一个年龄age，例如数字20
+做如下判断语句的编写：
+如果小于13岁，打印“小孩”
+如果大于等于13，并且小于18岁，打印“青少年”
+如果大于等于18，并且小于65岁，打印“成年人”
+如果大于等于65岁，打印“老年人”
+"""
+while True:
+    message = input("是否退出当前判断？如果退出，请输入'quit':")
+    if message == 'quit':
+        break
+    else:
+        age = int(input("请输入你要判断的年龄："))
+        if age < 13:
+            print("小孩")
+        elif age >= 13 and age < 18:
+            print("青少年")
+        elif age >= 18 and age < 65:
+            print("成年人")
+        else:
+            print("老年人")
+
