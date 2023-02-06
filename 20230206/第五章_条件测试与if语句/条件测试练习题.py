5cc222d38c0844eb51aff772ecf445d6c26f5542
michael.wan <michael.wan@ichub.com>
2023-02-06 17:19:38 +0800

    2023-02-06 17:19 if条件判断练习提交

--- 20230206/第五章_条件测试与if语句/条件测试练习题.py
+++ 20230206/第五章_条件测试与if语句/条件测试练习题.py
@@ -11,41 +11,53 @@
 判断成绩数字是否很顺口：成绩等于66分、或者成绩等于88分、或者成绩等于99分
 判断成绩及格并且是否是0结尾：判断成绩大于等于60，并且为60/70/80/90/100中的一个
 """
-grade = int(input("请输入你本次考试的成绩："))
-if grade == 100:
-    print("太棒了，你考了满分！")
-elif grade >= 60:
-    print(f"你的成绩合格，分数为:", grade)
-else:
-    print(f"很遗憾，你的成绩不合格。你的成绩是：", grade)
+# 方法一、直接进行条件判断，使用print函数进行结果输出
+grade = 66
+print("是否及格:", grade >= 60)
+print("是否不及格:", grade < 60)
+print("是否满分:", grade == 100)
+print("是否不是满分:", grade != 100)
+print("判断属于的中等成绩:", grade >= 70 and grade < 90)
+print("判断成绩数字是否很顺口:", grade == 66 or grade == 88 or grade==99)
+print("判断成绩数字是否很顺口:", grade in [66, 88, 99])
+print("判断成绩及格并且是否是0结尾:", grade >= 60 and grade in [60, 70, 80, 90, 100])
 
-grade = int(input("请输入你本次考试的成绩："))
-if grade != 100:
-    print("你的成绩还需要努力，你的分数是：", grade)
-
-print("*" * 10)
-grade = int(input("请输入你本次考试的成绩："))
-if grade >= 90:
-    print("你的成绩属于优秀，分数是：", grade)
-elif grade >= 70 and grade < 90:
-    print("你的成绩属于中等，分数是：", grade)
-elif grade >= 60 and grade < 70:
-    print("你的成绩刚合格，分数是：", grade)
-else:
-    print("你的成绩不合格，分数是：", grade)
-
-print("*" * 10)
-grade = int(input("请输入你本次考试的成绩："))
-decade = int(grade / 10)
-unit = grade % 10
-print(decade,unit)
-if decade == unit:
-    print("你的成绩很顺口，分数是：", grade)
-else:
-    print("你的分数是：", grade)
-
-print("*" * 10)
-grade = int(input("请输入你本次考试的成绩："))
-number = grade % 10
-if grade >= 60 and number == 0:
-    print("你的成绩合格，且为10的整数倍。你的分数是：", grade)
+# # 方法二、使用if进行条件判断
+# grade = int(input("请输入你本次考试的成绩："))
+# if grade == 100:
+#     print("太棒了，你考了满分！")
+# elif grade >= 60:
+#     print(f"你的成绩合格，分数为:", grade)
+# else:
+#     print(f"很遗憾，你的成绩不合格。你的成绩是：", grade)
+#
+# grade = int(input("请输入你本次考试的成绩："))
+# if grade != 100:
+#     print("你的成绩还需要努力，你的分数是：", grade)
+#
+# print("*" * 10)
+# grade = int(input("请输入你本次考试的成绩："))
+# if grade >= 90:
+#     print("你的成绩属于优秀，分数是：", grade)
+# elif grade >= 70 and grade < 90:
+#     print("你的成绩属于中等，分数是：", grade)
+# elif grade >= 60 and grade < 70:
+#     print("你的成绩刚合格，分数是：", grade)
+# else:
+#     print("你的成绩不合格，分数是：", grade)
+#
+# print("*" * 10)
+# grade = int(input("请输入你本次考试的成绩："))
+# decade = int(grade / 10)
+# unit = grade % 10
+# print(decade,unit)
+# if decade == unit:
+#     print("你的成绩很顺口，分数是：", grade)
+# else:
+#     print("你的分数是：", grade)
+#
+# print("*" * 10)
+# grade = int(input("请输入你本次考试的成绩："))
+# number = grade % 10
+# if grade >= 60 and number == 0:
+#     print("你的成绩合格，且为10的整数倍。你的分数是：", grade)
