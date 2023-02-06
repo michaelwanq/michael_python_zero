309643678a2d6b03fea6ff61eef24c78734d45d7
michael.wan <michael.wan@ichub.com>
2023-02-06 11:02:34 +0800

    2023-02-06 11:02 练习提交

--- 20230206/第二章_字符串变量和注释/变量练习题.py
+++ 20230206/第二章_字符串变量和注释/变量练习题.py
@@ -2,15 +2,15 @@
 # 新建一个变量name，值为"小明",打印它的值
 # 将name的值改为“小李”，打印name的值
 # 将name改成小张，打印name的值
-## 方法一
-# name = "小明"
-# print(name)
-# name = "小李"
-# print(name)
-# name = "小张"
-# print(name)
+# 方法一
+name = "小明"
+print(name)
+name = "小李"
+print(name)
+name = "小张"
+print(name)
 
-## 方法二
-names = ['小明','小李','小张']
-for name in names:
-    print(name)
+# ## 方法二
+# names = ['小明','小李','小张']
+# for name in names:
+#     print(name)
