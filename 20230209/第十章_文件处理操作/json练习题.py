c06e4534ac18803d829880429f351b6d0b33dda2
michael.wan <michael.wan@ichub.com>
2023-02-21 06:27:01 +0800

    2023-02-21 6:25 commit

--- 20230209/第十章_文件处理操作/json练习题.py
+++ 20230209/第十章_文件处理操作/json练习题.py
@@ -1,7 +1,7 @@
 """
 学生成绩录入与保存程序
 新建代码“成绩录入json.py”
-加载文件的词典文件：成绩录入json.txt
+加载文件的词典文件：json.txt
 如果文件存在，加载到字典 grade_dict
 如果文件不存在，新建一个成绩字典，例如grade_dict = {}
 while true，输入多个成绩
@@ -9,6 +9,33 @@ while true，输入多个成绩
 使用str.split拆分成name和grade
 存入字典中，name是Key，grade是Value
 输入quit，退出循环
-将grade_dict更新保存到json文件中：成绩录入json.txt
+将grade_dict更新保存到json文件中：json.txt
 类似用f.write(json.dumps(grade_dict))即可
 """
+import os
+import json
+
+file_name = "成绩录入json.txt"
+grade_dict = {}
+if os.path.exists(file_name):
+    with open(file_name, encoding="utf8") as f:
+        grade_dict = json.loads(f.read())
+
+print("### 读取文件后的内容")
+for key, value in grade_dict.items():
+    print(key, value)
+
+while True:
+    print("#" * 20)
+    info = input("请输入姓名和成绩：")
+    if info == "quit":
+        break
+    fields = info.split()
+    if len(fields) != 2:
+        continue
+    name, grade = fields
+    grade = int(grade)
+    grade_dict[name] = grade
+
+with open(file_name, "w", encoding="utf8") as f:
+    f.write(json.dumps(grade_dict))
